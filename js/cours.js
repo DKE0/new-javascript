968f121901505483dec171072f68b6b51627c103
DKE0 <danvidekodjoespoir@gmail.com>
2018-09-09 19:59:18 +0200

    add of little program and pass on linux

--- js/cours.js
+++ js/cours.js
@@ -143,8 +143,6 @@ console.log(heure2+"h "+minute2+"m "+seconde2+"s ");*/
 
 // program to build triangle with stars
 
-
-
 var x, y;
 x= NumberCheck(Number(prompt("Entrez les Coordonées X  ")));
 y= NumberCheck(Number(prompt("Entrez les Coordonées Y  ")));
@@ -173,3 +171,153 @@ function NumberCheck(input)
     else return input;
     
 }
+
+
+
+
+
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+//console.log("(La solution est " + solution + ")");
+
+// TODO : complétez le programme
+
+var tentative = 0 ;
+var nbUtil;
+var win= false;
+
+while((tentative<6) && (win==false))
+
+{
+    do
+    {
+        nbUtil = Number(prompt(" Entrez un nombre "));
+    }
+    while (isNaN(nbUtil)) ;
+    
+    if (nbUtil < solution ) console.log(nbUtil + " est trop petit ");
+    if (nbUtil > solution ) console.log(nbUtil + " est trop grand ");
+    if (nbUtil === solution) {
+        console.log("Bravo ! La solution Était " + nbUtil);
+        win = true;
+    }
+    tentative++;
+}
+
+
+/*do
+{
+    nbUtil = Number(prompt(" Entrez un nombre "));
+}
+while (isNaN(nbUtil)) ;*/
+
+
+/*var perso = {
+     nom:"Aurora",
+     sante:150,
+     force:25,
+    
+decrire: function(){
+    var description = this.nom + " a " + this.sante + " points de vie " + this.force + " en force";
+    return description;
+}
+    
+};
+
+console.log(perso.decrire());
+
+perso.sante = perso.sante - 20;
+
+perso.force = perso.force + 10;
+
+console.log(perso.decrire());
+*/
+
+var compte = {
+solde: 0,
+titulaire: "Alex",
+
+    crediter : function(nbUtil) {
+        this.solde+=nbUtil;
+    },
+    
+debiter: function (nbUtil) {
+    this.solde -=nbUtil;
+},
+    
+decrire: function(){
+    console.log("Titulaire : " + this.titulaire + " solde : " + this.solde);
+}
+    
+}
+
+do
+{
+    nbUtil = Number(prompt(" Montant à crediter "));
+}
+while (isNaN(nbUtil)) ;
+compte.crediter(nbUtil);
+do
+{
+    nbUtil = Number(prompt(" Montant à debiter "));
+}
+while (isNaN(nbUtil)) ;
+
+compte.debiter(nbUtil);
+compte.decrire();
+
+
+
+var perso = {
+    
+nom: "Aurora",
+    
+sante: 150,
+    
+force: 25,
+    
+xp: 0,
+    
+    
+    // Renvoie la description du personnage
+    
+decrire: function () {
+    
+    var description = this.nom + " a " + this.sante + " points de vie, " +
+    
+    this.force + " en force et " + this.xp + " points d'expérience";
+    
+    return description;
+    
+}
+    
+};
+
+
+console.log(perso.decrire());
+
+
+// Aurora est blessée par une flèche
+
+perso.sante = perso.sante - 20;
+
+
+// Aurora trouve un bracelet de force
+
+perso.force = perso.force + 10;
+
+
+// Aurora apprend une nouvelle compétence
+
+perso.xp = perso.xp + 15;
+
+
+console.log(perso.decrire());

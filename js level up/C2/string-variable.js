0327717db40e90b750ec9eb833f42af75d465a35
DKE0 <danvidekodjoespoir@gmail.com>
2018-10-27 01:09:26 +0200

    basic js with cool css

---
+++ js level up/C2/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = "Molly";
+message = "See our upcoming range";
+
+var elname =  document.getElementById('name');
+elname.textContent = username;
+var elnote = document.getElementById('note');
+elnote.textContent = message;
